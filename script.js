77e70b0facea5b8d73b3020c8f3ef2f4aefb060d
harpreetsgill <harpreet_singh_gill@outlook.com>
2020-11-14 02:31:55 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-var request = new XMLHttpRequest();
+// var request = new XMLHttpRequest();
 
-request.open('GET', 'http://newsapi.org/v2/top-headlines?country=us&apiKey=b487fd06bd424e288f82cec72ff4977b');
+// request.open('GET', 'http://newsapi.org/v2/top-headlines?country=us&apiKey=b487fd06bd424e288f82cec72ff4977b');
 
-request.onload = function() {
-    var response = request.response;
-    var parsedData = JSON.parse(response);
-    console.log(parsedData);
+// request.onload = function() {
+//     var response = request.response;
+//     var parsedData = JSON.parse(response);
+//     console.log(parsedData);
     
-}
+// }
 
-request.send();
+// request.send();
+
+fetch('http://newsapi.org/v2/top-headlines?country=us&apiKey=b487fd06bd424e288f82cec72ff4977b')
+    .then(res => res.json())
+    .then(data => console.log(data));
